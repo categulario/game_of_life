9a509099bd7430c241d9b052c1caa59db2dd8a19
Abraham Toriz <awonderfulcode@gmail.com>
2018-03-07 01:02:45 -0600

    be able to draw from file

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use piston_window::*;
 
-fn render(window:&mut PistonWindow, event:Event, data:&[i32], width:i32) {
+fn render(window:&mut PistonWindow, event:Event, data:&[u32], width:u32) {
     window.draw_2d(&event, |context, graphics| {
         clear([1.0; 4], graphics);
 
-        for h in 0..(data.len() as i32)/width {
+        for h in 0..(data.len() as u32)/width {
             for w in 0..width {
                 if data[(h*width + w) as usize] == 1 {
                     rectangle([0.0, 0.0, 0.0, 1.0],
@@ -21,6 +21,34 @@ fn render(window:&mut PistonWindow, event:Event, data:&[i32], width:i32) {
             }
         }
     });
+}
+
+fn data_from_file(filename:&str, width:u32, height:u32) -> Vec<u32> {
+    let f = File::open(filename).expect("Unable to open file");
+    let f = BufReader::new(f);
+    let mut arr = vec![0; (width*height) as usize];
+
+    for (h, line) in f.lines().enumerate() {
+        if (h as u32) == height {
+            break;
+        }
+
+        let line = line.expect("Something happened while reading a line from the file");
+
+        for (w, c) in line.chars().enumerate() {
+            if (w as u32) == width {
+                break;
+            }
+
+            if c == 'x' {
+                arr[h*(width as usize) + w] = 1;
+            }
+        }
+
+        println!("Line: {}", line);
+    }
+
+    return arr;
 }
 
 fn main() {
@@ -48,15 +76,7 @@ fn main() {
            .takes_value(true))
         .get_matches();
 
-    // let world_filename = matches.value_of("FILE").unwrap();
-
-    // let f = File::open(world_filename).expect("Unable to open file");
-    // let f = BufReader::new(f);
-
-    // for line in f.lines() {
-        // let line = line.expect("Something happening while reading the line");
-        // println!("Line: {}", line);
-    // }
+    let world_filename = matches.value_of("FILE").unwrap();
 
     let width:u32 = matches.value_of("width").unwrap().parse().expect("Need an integer for width");
     let height:u32 = matches.value_of("height").unwrap().parse().expect("Need an integer for height");
@@ -71,12 +91,9 @@ fn main() {
             .unwrap(),
     );
 
-    let data = [
-        1, 0, 1,
-        0, 1, 0,
-    ];
+    let data = data_from_file(world_filename, width, height);
 
     while let Some(event) = window.next() {
-        render(&mut window, event, &data, 3);
+        render(&mut window, event, &data, width);
     }
 }
