1cd9a7049dd4d4a9390d45dac00954d3b363dc2c
Abraham Toriz <awonderfulcode@gmail.com>
2018-03-06 20:59:35 -0600

    be able to process command lines arguments

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
-extern crate piston_window;
+extern crate clap;
 
-use piston_window::*;
+use clap::{Arg, App};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
-    let mut window: PistonWindow = PistonWindow::new(
-        OpenGL::V3_3,
-        0,
-        WindowSettings::new("Hello World!", [640, 480])
-            .opengl(OpenGL::V3_3)
-            .srgb(false)
-            .build()
-            .unwrap(),
-    );
+    let matches = App::new("Game of life")
+        .version("0.1.0")
+        .author("Abraham Toriz <categulario@gmail.com>")
+        .about("Runs a simulation of the game of life")
+        .arg(Arg::with_name("FILE")
+           .help("File that states initial status of the world")
+           .required(true)
+           .index(1))
+        .get_matches();
 
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics| {
-            clear([1.0; 4], graphics);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      context.transform,
-                      graphics);
-        });
+    let world_filename = matches.value_of("FILE").unwrap();
+
+    let f = File::open(world_filename).expect("Unable to open file");
+    let f = BufReader::new(f);
+
+    for line in f.lines() {
+        let line = line.expect("Something happening while reading the line");
+        println!("Line: {}", line);
     }
 }
